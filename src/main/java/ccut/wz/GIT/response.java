edc2abd6d80563471d85ca3d1846b99791bf2550
wz <1835187730@qq.com>
2022-07-28 20:30:54 +0800

    third commit

--- src/main/java/ccut/wz/GIT/response.java
+++ src/main/java/ccut/wz/GIT/response.java
@@ -4,5 +4,6 @@ public class response {
     public static void main(String[] args) {
     System.out.println("hello,world~");
     System.out.println("hello,world222~");
+    System.out.println("hello,world222~");
     }
 }
