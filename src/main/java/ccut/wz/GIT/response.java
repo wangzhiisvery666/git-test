3cf409e0875b3e5065997a951ce1fc86e2fb8a63
wz <1835187730@qq.com>
2022-07-28 20:40:38 +0800

    hot-fix first commit

--- src/main/java/ccut/wz/GIT/response.java
+++ src/main/java/ccut/wz/GIT/response.java
@@ -5,5 +5,6 @@ public class response {
     System.out.println("hello,world~");
     System.out.println("hello,world222~");
     System.out.println("hello,world222~");
+    System.out.println("hello,world444~");
     }
 }
