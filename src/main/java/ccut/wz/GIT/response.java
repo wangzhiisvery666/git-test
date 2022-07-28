e98c7aa966dc08f22bfb41e9e68e74da81c09566
wz <1835187730@qq.com>
2022-07-28 20:15:30 +0800

    second commit

---
+++ src/main/java/ccut/wz/GIT/response.java
@@ -0,0 +1,8 @@
+package ccut.wz.GIT;
+
+public class response {
+    public static void main(String[] args) {
+    System.out.println("hello,world~");
+    System.out.println("hello,world222~");
+    }
+}
