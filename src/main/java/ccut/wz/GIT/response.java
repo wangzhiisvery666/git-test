f65e7357cd98edd59aeb1ca915f268cfcb333748
xingzejiangzhi <100738284+xingzejiangzhi@users.noreply.github.com>
2022-07-28 21:29:01 +0800

    pull2

--- src/main/java/ccut/wz/GIT/response.java
+++ src/main/java/ccut/wz/GIT/response.java
@@ -10,5 +10,6 @@ public class response {
     System.out.println("hot-fix merge");
     System.out.println("push test~");
     System.out.println("pull test~");
+         System.out.println("pull2 test~");
     }
 }
